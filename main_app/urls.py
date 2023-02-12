5f1c7b21d5b1198935ae25265f4c3a8838b70a9b
Nikita Nikitov <c701@list.ru>
2023-02-13 00:46:39 +0300

    Изменено описание модуля, добавлен адрес для API

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,30 +1,18 @@
-"""main_site URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-	https://docs.djangoproject.com/en/4.1/topics/http/urls/
-
-Examples:
-
-Function views
-	1. Add an import:  from my_app import views
-	2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-	1. Add an import:  from other_app.views import Home
-	2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-	1. Import the include() function: from django.urls import include, path
-	2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+Модуль для привязки url к методам обработки
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
-from .views import index_page, login_user, logout_user, profile, register_user
+from .views import (generate_qr, index_page, login_user, logout_user, profile, register_user)
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
 	path('', index_page),
-	path('logout/', logout_user),
-	path('login/', login_user),
+	path('logout/', logout_user, name='logout'),
+	path('login/', login_user, name='login'),
 	path('signup/', register_user),
 	path('profile/', profile),
+	path('qr/', generate_qr, name='generate_qr'),
+	path('api/', include('api.urls'))
 ]
