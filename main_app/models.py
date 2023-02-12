4d4c7a81d85d919328aff58ed3ae629d481fe7d1
Nikita Nikitov <c701@list.ru>
2023-02-13 00:48:13 +0300

    Добавлен метод status_display, добавлена модель QRCode

--- main_app/models.py
+++ main_app/models.py
@@ -1,12 +1,12 @@
 """
 Модуль для создании моделей для базы данных
 """
+from datetime import datetime
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 STATUSES = (('Student', 'Студент'), ('Teacher', 'Преподаватель'))
-
-# Create your models here.
 
 
 class CustomUser(AbstractUser):
@@ -35,3 +35,31 @@ class CustomUser(AbstractUser):
 		данный пользователь - преподователем
 		"""
 		return self.status == 'Teacher'
+
+	def status_display(self) -> str:
+		"""
+		Функция, которая возращает статус студента в
+		текстовом виде для отображения
+		"""
+		return self.get_status_display()  # type: ignore # Рантайм функция от Django
+
+
+class QRCode(models.Model):
+	"""
+	Таблица с QR кодами пользователей
+	"""
+	user = models.OneToOneField(
+		CustomUser, on_delete=models.CASCADE, primary_key=True, verbose_name="Пользователь"
+	)
+	code = models.CharField(max_length=16, unique=True, verbose_name="Код")
+	time_start = models.DateTimeField(verbose_name="Срок начала действия кода")
+	time_expire = models.DateTimeField(
+		default=datetime.now(), verbose_name="Срок истечения действия кода"
+	)
+
+	class Meta:
+		verbose_name = "QR Код"
+		verbose_name_plural = "QR Коды"
+
+	def __str__(self) -> str:
+		return f'{self.user} - {self.code}'
