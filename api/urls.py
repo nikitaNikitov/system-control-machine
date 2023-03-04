c987cb70bd002bdc00eaedde0cfcadb8fea79e8d
Nikita Nikitov <c701@list.ru>
2023-03-05 01:20:35 +0300

    Добавлены новые методы в api

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,21 @@
 """
 from django.urls import path
 
-from .views import get_qr, get_user_from_code
+from .views import (
+	get_qr,
+	get_user_from_code,
+	get_user_from_code_machine,
+	give_permission,
+	manage,
+	manage_machine,
+)
 
 urlpatterns = [
 	path('getUser', get_user_from_code),
-	path('qr', get_qr)
+	path('getUser/<machine>', get_user_from_code_machine),
+	path('qr', get_qr),
+	path('users', manage),
+	path('users/<machine>', manage_machine),
+	path('givePermission', give_permission),
+	# path('revokePermission', revoke_permission),
 ]
