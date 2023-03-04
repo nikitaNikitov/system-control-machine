0dccaae3141c08c718e9f7103f11ed1f909a3dc6
Nikita Nikitov <c701@list.ru>
2023-03-05 01:14:30 +0300

    Заменен datetime, добавлены методы

--- api/utils.py
+++ api/utils.py
@@ -1,7 +1,9 @@
 """
 Модуль различных утилит для использования в коде
 """
-import datetime
+from enum import Enum
+import json
+import re
 import secrets
 import string
 from time import time
@@ -9,10 +11,19 @@ from typing import Any
 
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import QueryDict
+from django.utils import timezone
 
 from main_app.models import QRCode
 
 SYMBVOLS = string.ascii_letters + string.digits
+
+
+class Error(Enum):
+	"""
+	Класс сообщений для ошибок
+	"""
+	USER_NOT_FOUND = 'Пользователь не найден!'
+	USER_NOT_TEACHER = 'Пользователь не является преподавателем!'
 
 
 def get_params(request: WSGIRequest) -> QueryDict:
@@ -44,8 +55,8 @@ def generate_code(user) -> dict[str, Any]:
 		usercode.delete()
 
 	code = ''.join(secrets.choice(SYMBVOLS) for _ in range(16))
-	time_start = datetime.datetime.now() + datetime.timedelta(seconds=5)
-	time_expire = datetime.datetime.now() + datetime.timedelta(minutes=5, seconds=5)
+	time_start = timezone.now() + timezone.timedelta(seconds=5)
+	time_expire = timezone.now() + timezone.timedelta(minutes=5, seconds=5)
 
 	QRCode(
 		user=user,
@@ -60,4 +71,38 @@ def generate_code(user) -> dict[str, Any]:
 		'time_expire': int(time_expire.timestamp()),
 	}
 	return data
-	
+
+
+ENGLISH_PATTERN = '[A-z0-9]+'
+RUSSIAN_PATTERN = '[А-яЁё]+'
+
+
+def has_only_english_and_number_letters(name) -> bool:
+	"""
+	Проверяет, состоит ли строка только из английских букв и цифр
+	"""
+	return bool(re.fullmatch(ENGLISH_PATTERN, name))
+
+
+def has_only_russian_letters(name) -> bool:
+	"""
+	Проверяет, состоит ли строка только из русских букв
+	"""
+	return bool(re.fullmatch(RUSSIAN_PATTERN, name))
+
+
+def check_access_from_user(user: dict, machine_name: str) -> bool | dict:
+	if 'data' in user:
+		return check_access_from_data(json.loads(user['data']), machine_name)
+	return False
+
+
+def check_access_from_data(data: dict, machine_name: str) -> bool | dict:
+	if 'permission' not in data or machine_name not in data['permission']:
+		return False
+	data = data['permission'][machine_name]
+	now = timezone.now().timestamp()
+	print(now)
+	if data['time_start'] <= now <= data['time_end']:
+		return data
+	return False
